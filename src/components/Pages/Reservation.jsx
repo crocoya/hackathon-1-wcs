fa6ff0407f542ac6f31fa50d4c1b71804ad33cfa
Janssen <jeanbaptiste.janssen10@gmail.com>
2021-05-11 23:57:31 +0200

    Ajout des bouttons dans la page réservation

--- src/components/Pages/Reservation.jsx
+++ src/components/Pages/Reservation.jsx
@@ -3,8 +3,22 @@ import './Reservation.css';
 
 export default function Reservation() {
   return (
-    <>
-      <div className='container-reservation'></div>
-    </>
+    <div className='container-reservation'>
+      <div className='placementBoutton'>
+        <button type='button' className='style-boutton'>
+          SpaceShip
+        </button>
+        <div className='placementBoutton2'>
+          <button type='button' className='style-boutton2'>
+            Planet
+          </button>
+          <div className='placementBoutton3'>
+            <button type='button' className='style-boutton3'>
+              Départ
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
