b64e77d6171eed17062808ac147b9aa7e2b7f679
Jb10-0 <74372841+JbWCS@users.noreply.github.com>
2021-05-12 09:56:10 +0200

    Changement des bouttons et mise en place du fond home

--- src/components/Pages/Reservation.jsx
+++ src/components/Pages/Reservation.jsx
@@ -1,20 +1,24 @@
-import React, { useEffect, useState } from 'react';
-import UserService from '../Service/user-service';
+import React from 'react';
 import './styles/Reservation.css';
 
 export default function Reservation() {
-  const [currentUser, setCurrentUser] = useState();
-
-  useEffect(() => {
-    UserService.getCurrentUser();
-    setCurrentUser(UserService.user);
-  }, []);
   return (
-    <>
-      <div className='container-reservation'>
-        {currentUser && <div>{currentUser.pseudo}</div>}
-        
+    <div className='container-reservation'>
+      <div className='placementBoutton'>
+        <button type='button' className='style-boutton'>
+          SpaceShip
+        </button>
+        <div className='placementBoutton2'>
+          <button type='button' className='style-boutton2'>
+            Planet
+          </button>
+          <div className='placementBoutton3'>
+            <button type='button' className='style-boutton3'>
+              Départ
+            </button>
+          </div>
+        </div>
       </div>
-    </>
+    </div>
   );
 }
