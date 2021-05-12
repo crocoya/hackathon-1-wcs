b2eb2fc8a6fc60e6ea1fa177b3a8a0d10f58d74e
Crocoya <yassine.tababi@outlook.com>
2021-05-12 16:34:59 +0200

    fin

--- src/components/Pages/Reservation.jsx
+++ src/components/Pages/Reservation.jsx
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './styles/Reservation.css';
 import TransferList from './TransferList';
+import UserService from '../Service/user-service';
 
 export default function Reservation() {
+  const [currentUser, setCurrentUser] = useState(); 
+
+  useEffect(() => {
+    UserService.getCurrentUser();
+    setCurrentUser(UserService.user);
+  }, []);
+
   return (
     <div className='container-reservation'>
       <div className='postionList'>
-        <h1 className="title-reserv">TO THE MOON</h1>
+        <h1 className="title-reserv">TO THE MOON {currentUser && <div className="elon-title"> { currentUser.pseudo}</div>}</h1>
         <TransferList />
       </div>
     </div>
