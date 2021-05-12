99cfb12a91b404cf0ab897f5da51fa05e08c1feb
Crocoya <yassine.tababi@outlook.com>
2021-05-12 12:56:41 +0200

    correction

--- src/components/Pages/Reservation.jsx
+++ src/components/Pages/Reservation.jsx
@@ -5,23 +5,9 @@ import TransferList from './TransferList';
 export default function Reservation() {
   return (
     <div className='container-reservation'>
-      <div className='placementBoutton'>
-        <button type='button' className='style-boutton'>
-          SpaceShip
-        </button>
-        <div className='placementBoutton2'>
-          <button type='button' className='style-boutton2'>
-            Planet
-          </button>
-          <div className='placementBoutton3'>
-            <button type='button' className='style-boutton3'>
-              Départ
-            </button>
-            <div className='postionList'>
-              <TransferList />
-            </div>
-          </div>
-        </div>
+      <div className='postionList'>
+        <h1 className="title-reserv">RESERVEZ VOTRE VOYAGE INOUBLIABLE</h1>
+        <TransferList />
       </div>
     </div>
   );
